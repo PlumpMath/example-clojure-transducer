6f75664b130f779e56667fd9ac89cbf4931b4bfa
Jinseop Kim <vmfhrmfoaj@yahoo.com>
2015-03-07 23:20:38 +0900

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject examples-transducer "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "GPLv2"
+            :url "https://www.gnu.org/licenses/gpl-2.0.html"}
+  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]]
+  :main ^:skip-aot examples-transducer.core)
